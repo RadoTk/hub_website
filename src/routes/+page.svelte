dd5f4b10a7bdf7cfb410e105ddaed93d0e994f94
RadoTk <hei.rado.3@gmail.com>
2026-02-19 14:38:24 +0300

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class="text-3xl font-bold text-center">Welcome to SvelteKit</h1>
+<p class="text-center">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
